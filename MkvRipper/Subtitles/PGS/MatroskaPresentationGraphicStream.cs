1802079c8e0f308c97ec57a1205c9841170ac489
David Schulte <mail@david-schulte.de>
2024-08-17 14:32:40 +0200

    Also return simple blocks for subtitle data from the MKV file.

--- MkvRipper/Subtitles/PGS/MatroskaPresentationGraphicStream.cs
+++ MkvRipper/Subtitles/PGS/MatroskaPresentationGraphicStream.cs
@@ -33,32 +33,59 @@ public class MatroskaPresentationGraphicStream : IPresentationGraphicStream
         
         var timestampScale = _matroska.Segment.Info.TimestampScale / 1_000_000.0;
         
+        foreach (var (timestamp, block) in ReadBlocksForTrackNumber())
+        {
+            if (block.TrackNumber != _trackNumber) continue;
+            if (block.Data is null) continue;
+
+            var timeInMilliseconds =
+                (timestamp + (double)block.TimeCode) * timestampScale - track.CodecDelay;
+
+            await using var ms = new MemoryStream(block.Data);
+            using var reader = new BigEndianBinaryReader(ms);
+            var displaySet = new DisplaySet();
+
+            var pgsTimestamp = (uint)(timeInMilliseconds * 90);
+            displaySet.PresentationTimestamp = pgsTimestamp;
+            // Decoding time isn't used and set to 0 most of the time. FFmpeg sets it to the presentation
+            // timestamp, so I'll do it as well.
+            displaySet.DecodingTimestamp = pgsTimestamp;
+            displaySet.Read(reader, includeHeader: false);
+
+            yield return displaySet;
+        }
+    }
+
+    /// <summary>
+    /// Returns all relevant blocks for the current track number.
+    /// </summary>
+    /// <returns>Returns the timestamps and block data.</returns>
+    private IEnumerable<(ulong, Block)> ReadBlocksForTrackNumber()
+    {
         if (_matroska.Segment.Clusters is null) yield break;
         foreach (var cluster in _matroska.Segment.Clusters)
         {
-            if (cluster.BlockGroups is null) continue;
-            foreach (var blockGroup in cluster.BlockGroups)
+            if (cluster.BlockGroups is not null)
             {
-                foreach (var block in blockGroup.Blocks)
+                foreach (var blockGroup in cluster.BlockGroups)
+                {
+                    foreach (var block in blockGroup.Blocks)
+                    {
+                        if (block.TrackNumber != _trackNumber) continue;
+                        if (block.Data is null) continue;
+
+                        yield return (cluster.Timestamp, block);
+                    }
+                }
+            }
+            
+            if (cluster.SimpleBlocks is not null)
+            {
+                foreach (var block in cluster.SimpleBlocks)
                 {
                     if (block.TrackNumber != _trackNumber) continue;
                     if (block.Data is null) continue;
-
-                    var timeInMilliseconds =
-                        (cluster.Timestamp + (double)block.TimeCode) * timestampScale - track.CodecDelay;
-
-                    await using var ms = new MemoryStream(block.Data);
-                    using var reader = new BigEndianBinaryReader(ms);
-                    var displaySet = new DisplaySet();
-
-                    var pgsTimestamp = (uint)(timeInMilliseconds * 90);
-                    displaySet.PresentationTimestamp = pgsTimestamp;
-                    // Decoding time isn't used and set to 0 most of the time. FFmpeg sets it to the presentation
-                    // timestamp, so I'll do it as well.
-                    displaySet.DecodingTimestamp = pgsTimestamp; 
-                    displaySet.Read(reader, includeHeader: false);
-                    
-                    yield return displaySet;
+                    yield return (cluster.Timestamp, block);
                 }
             }
         }
